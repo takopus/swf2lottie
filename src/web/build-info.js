14d6d58ad59ba385e866b6f6e42722dd47c8437f
takopus <takopus@gmail.com>
2026-03-24 14:55:54 +0300

    Fixed build info

--- src/web/build-info.js
+++ src/web/build-info.js
@@ -1,4 +1,4 @@
 export const APP_VERSION = "0.9.0";
 export const BUILD_NUMBER = 81;
-export const BUILD_STAMP = "1336-240324";
+export const BUILD_STAMP = "1455-240326";
 export const BUILD_LABEL = `v${APP_VERSION} build ${BUILD_NUMBER}-${BUILD_STAMP}`;
