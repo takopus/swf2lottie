2b7b76a757505915bc8aed6b60ff289558a83e36
takopus <takopus@gmail.com>
2026-03-24 14:02:08 +0300

    Show version in build badge

--- src/web/build-info.js
+++ src/web/build-info.js
@@ -1,3 +1,4 @@
-export const BUILD_NUMBER = 79;
-export const BUILD_STAMP = "1332-240324";
-export const BUILD_LABEL = `build ${BUILD_NUMBER}-${BUILD_STAMP}`;
+export const APP_VERSION = "0.9.0";
+export const BUILD_NUMBER = 81;
+export const BUILD_STAMP = "1336-240324";
+export const BUILD_LABEL = `v${APP_VERSION} build ${BUILD_NUMBER}-${BUILD_STAMP}`;
